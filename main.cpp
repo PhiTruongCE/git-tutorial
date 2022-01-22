dee0b34e8ee451d5debc07b66d538a575d95c731
Truong <truongvo112k@gmail.com>
2022-01-22 09:29:00 +0700

    add all file

---
+++ main.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+void swap(int &a,int &b){
+    a = a + b;
+    b = a - b;
+    a = a - b;
+}
+
+int main(){
+    int a = 3;
+    int b = 4;
+    swap(a,b);
+    cout<<a<<" "<<b;
+    return 0;
+}
---
+++ test.c
@@ -0,0 +1,4 @@
+#include "header.h"
+
+int var = 100;
+int array[] = {1,2,3,4,5};
